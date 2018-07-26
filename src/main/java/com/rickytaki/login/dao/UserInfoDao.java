7e70e730c0191ed77364e7e5a79843271e4f7abe
rickytaki <me@rickytaki.com>
2018-07-25 23:41:18 -0300

    Remove unecesary public to follow best practices

--- src/main/java/com/rickytaki/login/dao/UserInfoDao.java
+++ src/main/java/com/rickytaki/login/dao/UserInfoDao.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface UserInfoDao {
 
-    public void save (UserInfo userInfo);
-    public Optional<UserInfo> findByName (String name);
-    public Optional<UserInfo> findByEmail(String s);
+    void save (UserInfo userInfo);
+    Optional<UserInfo> findByName (String name);
+    Optional<UserInfo> findByEmail(String s);
 }
